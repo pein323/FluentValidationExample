e5ce05132000d51cc5ef0685d482424ee823837c
Krzysztof Bistram (PGS Software) <kbistram@pgs-soft.com>
2019-03-25 16:04:44 +0100

    Added model for Fluent Validation

---
+++ FluentValidationExample/FluentValidationExample/Models/UserModelFV.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentValidationExample.Models
+{
+    public class UserModelFV
+    {
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
