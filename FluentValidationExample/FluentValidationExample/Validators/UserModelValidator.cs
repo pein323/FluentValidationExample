2560349dd6d5f85f97cfbada4f6633f6b674cab9
Krzysztof Bistram (PGS Software) <kbistram@pgs-soft.com>
2019-03-26 10:23:51 +0100

    Improved fluent validation

--- FluentValidationExample/FluentValidationExample/Validators/UserModelValidator.cs
+++ FluentValidationExample/FluentValidationExample/Validators/UserModelValidator.cs
@@ -8,7 +8,17 @@ namespace FluentValidationExample.Validators
     {
         public UserModelValidator()
         {
-            RuleFor(user => user.UserName).NotNull();
+            RuleFor(user => user.UserName)
+                .NotNull();
+
+            RuleFor(user => user.Password)
+                .NotNull()
+                .Length(6,100)
+                .WithMessage("The {0} must be at least {2} characters long.");
+
+            RuleFor(user => user.ConfirmPassword)
+                .Equal(x => x.Password)
+                .WithMessage("The password and confirmation password do not match.");
         }
     }
 }
